c064d5bf6300808d6d5ca906eca8f2775a69d53e
YonyM <yonymartens@gmx.de>
2021-05-30 14:34:09 +0200

    test

--- aufgabe_3.1/server.ts
+++ aufgabe_3.1/server.ts
@@ -21,7 +21,7 @@ export namespace P_3_1Server {
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         _response.write(_request.url);
-        //console.log(_request.url);
+        console.log(_request.url);
         _response.end();
     }
 }

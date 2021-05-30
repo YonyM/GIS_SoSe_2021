82ab12feb5960f54b11e95b9aedf63342c9c2596
YonyM <yonymartens@gmx.de>
2021-05-30 14:27:41 +0200

    eigene url

--- aufgabe_3.1/server.ts
+++ aufgabe_3.1/server.ts
@@ -8,20 +8,20 @@ export namespace P_3_1Server {
 
     let server: Http.Server = Http.createServer();  //Erstellt Server
     server.addListener("request", handleRequest);   //hängt ein Listener für den Fall einer Serveranfrage an.
-    server.addListener("listening", handleListen);  
-    server.listen(port);  
+    server.addListener("listening", handleListen);
+    server.listen(port);
 
     function handleListen(): void {  //Wird 1x nach dem Start ausgeführt wenn der Server "zuhört"
         console.log("Listening");
     }
 
     // wird ausgeführt wenn der Server eine Anfrage bekommt
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { 
-        //console.log("I hear voices!");
-        //_response.setHeader("content-type", "text/html; charset=utf-8");
-        //_response.setHeader("Access-Control-Allow-Origin", "*");
-        //_response.write(_request.url);
-        console.log(_request.url);
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+        console.log("I hear voices!");
+        _response.setHeader("content-type", "text/html; charset=utf-8");
+        _response.setHeader("Access-Control-Allow-Origin", "*");
+        _response.write(_request.url);
+        //console.log(_request.url);
         _response.end();
     }
 }

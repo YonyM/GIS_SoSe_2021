bafb083f1e9f2cb1ee6919f4b48720f288901d71
YonyM <yonymartens@gmx.de>
2021-05-30 14:47:51 +0200

    test3

--- aufgabe_3.1/server.ts
+++ aufgabe_3.1/server.ts
@@ -22,6 +22,7 @@ export namespace P_3_1Server {
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         _response.write(_request.url);
+        _response.write("waruuuuuum????"); 
         console.log("WARUUUUUUUUM????");
         console.log(Url);
         _response.end();

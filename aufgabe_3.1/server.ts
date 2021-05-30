b1777bc062dc860f9285f4ef077de6b99a4ece2f
YonyM <yonymartens@gmx.de>
2021-05-30 14:12:30 +0200

    querystring

--- aufgabe_3.1/server.ts
+++ aufgabe_3.1/server.ts
@@ -1,26 +1,27 @@
-import * as Http from "http";
+import * as Http from "http"; //import sämtlicher Funktionalitäten von "http"
 
 export namespace P_3_1Server {
     console.log("Starting server");
     let port: number = Number(process.env.PORT);
-    if (!port)
+    if (!port)   //Checkt den Port
         port = 8100;
 
-    let server: Http.Server = Http.createServer();
-    server.addListener("request", handleRequest);
-    server.addListener("listening", handleListen);
-    server.listen(port);
+    let server: Http.Server = Http.createServer();  //Erstellt Server
+    server.addListener("request", handleRequest);   //hängt ein Listener für den Fall einer Serveranfrage an.
+    server.addListener("listening", handleListen);  
+    server.listen(port);  
 
-    function handleListen(): void {
+    function handleListen(): void {  //Wird 1x nach dem Start ausgeführt wenn der Server "zuhört"
         console.log("Listening");
     }
 
-
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
+    // wird ausgeführt wenn der Server eine Anfrage bekommt
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void { 
         console.log("I hear voices!");
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         _response.write(_request.url);
+        console.log(_request.url);
         _response.end();
     }
 }

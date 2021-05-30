bd3bde5ad25aff07baa0fb524cbccfa5df7b3d71
YonyM <yonymartens@gmx.de>
2021-05-30 14:54:37 +0200

    test4

--- aufgabe_3.1/server.ts
+++ aufgabe_3.1/server.ts
@@ -1,5 +1,5 @@
 import * as Http from "http"; //import sämtlicher Funktionalitäten von "http"
-import * as Url from "url";
+
 
 export namespace P_3_1Server {
     console.log("Starting server");
@@ -22,9 +22,7 @@ export namespace P_3_1Server {
         _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
         _response.write(_request.url);
-        _response.write("waruuuuuum????"); 
-        console.log("WARUUUUUUUUM????");
-        console.log(Url);
+        console.log(_request.url);
         _response.end();
     }
 }

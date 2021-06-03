c927b55e4483414b15e57ef48f537421a9101715
YonyM <yonymartens@gmx.de>
2021-06-03 12:17:09 +0200

    auführlichere Kommentare

--- aufgabe_3.1/server.ts
+++ aufgabe_3.1/server.ts
@@ -1,28 +1,28 @@
 import * as Http from "http"; //import sämtlicher Funktionalitäten von "http"
 
 
-export namespace P_3_1Server {
-    console.log("Starting server");
-    let port: number = Number(process.env.PORT);
-    if (!port)   //Checkt den Port
-        port = 8100;
+export namespace P_3_1Server { 
+    console.log("Starting server");    //Ausgabe zu Beginn des Serverstarts
+    let port: number = Number(process.env.PORT);    //Ist mir nicht ganz klar
+    if (!port)   
+        port = 8100;  // Setzt den Port auf 8100
 
-    let server: Http.Server = Http.createServer();  //Erstellt Server
-    server.addListener("request", handleRequest);   //hängt ein Listener für den Fall einer Serveranfrage an.
-    server.addListener("listening", handleListen);
-    server.listen(port);
+    let server: Http.Server = Http.createServer();  //Erstellt neuen http-Server
+    server.addListener("request", handleRequest);   //"request-listener" für einkommende Anfragen an den Server -> bei einer Anfrage wird die Funktion "handleRequest ausgeführt"
+    server.addListener("listening", handleListen);  //für den Serverstatus "Listening" wird durch dieses Listener die Funktion "handleListen" ausgeführt
+    server.listen(port); // horcht auf dem gewählten Port nach Anfragen
 
-    function handleListen(): void {  //Wird 1x nach dem Start ausgeführt wenn der Server "zuhört"
-        console.log("Listening");
+    function handleListen(): void {  //Wird nach dem Start ausgeführt wenn der Server "zuhört"
+        console.log("Listening");    // Zeigt mit der Ausgabe "Listening", dass der Server zuhört
     }
 
     // wird ausgeführt wenn der Server eine Anfrage bekommt
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-        console.log("I hear voices!");
-        _response.setHeader("content-type", "text/html; charset=utf-8");
+        console.log("I hear voices!");  //Wird ausgegeben sobald der Server von jemandem erreicht wurde und die handleRequest "aktiviert" wird
+        _response.setHeader("content-type", "text/html; charset=utf-8"); //Definiert die "Art" der response-Daten
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write(_request.url);
-        console.log(_request.url);
+        _response.write(_request.url); //Zeigt die individuelle request-URL an.
+        console.log(_request.url); //Gibt die individuelle request-URL aus.
         _response.end();
     }
 }

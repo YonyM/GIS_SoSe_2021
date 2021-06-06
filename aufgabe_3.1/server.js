98ee49e761e47de954de13d42f39c7e8696a628c
YonyM <yonymartens@gmx.de>
2021-06-06 14:52:02 +0200

    aufgabe3.2(3.1)

--- aufgabe_3.1/server.js
+++ aufgabe_3.1/server.js
@@ -2,12 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.P_3_1Server = void 0;
 const Http = require("http"); //import sämtlicher Funktionalitäten von "http"
+const Url = require("url");
 var P_3_1Server;
 (function (P_3_1Server) {
     console.log("Starting server"); //Ausgabe zu Beginn des Serverstarts
     let port = Number(process.env.PORT); //Ist mir nicht ganz klar
     if (!port)
-        port = 8100; // Setzt den Port auf 8100
+        port = 8101; // Setzt den Port auf 8100
     let server = Http.createServer(); //Erstellt neuen http-Server
     server.addListener("request", handleRequest); //"request-listener" für einkommende Anfragen an den Server -> bei einer Anfrage wird die Funktion "handleRequest ausgeführt"
     server.addListener("listening", handleListen); //für den Serverstatus "Listening" wird durch dieses Listener die Funktion "handleListen" ausgeführt
@@ -18,10 +19,21 @@ var P_3_1Server;
     // wird ausgeführt wenn der Server eine Anfrage bekommt
     function handleRequest(_request, _response) {
         console.log("I hear voices!"); //Wird ausgegeben sobald der Server von jemandem erreicht wurde und die handleRequest "aktiviert" wird
-        _response.setHeader("content-type", "text/html; charset=utf-8"); //Definiert die "Art" der response-Daten
+        _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write(_request.url); //Zeigt die individuelle request-URL an.
-        console.log(_request.url); //Gibt die individuelle request-URL aus.
+        let urlparse = Url.parse(_request.url, true);
+        let url = new URL(_request.url, "https://yonysgisserver.herokuapp.com");
+        if (url.pathname == "/html") {
+            for (let key in urlparse.query) {
+                console.log("html");
+                _response.write("<p>" + key + ":" + urlparse.query[key] + "<p/>");
+            }
+        }
+        else if (url.pathname == "/json") {
+            console.log("json");
+            let antwortJson = JSON.stringify(urlparse.query);
+            _response.write(antwortJson);
+        }
         _response.end();
     }
 })(P_3_1Server = exports.P_3_1Server || (exports.P_3_1Server = {}));
--- aufgabe_3.1/server.ts
+++ aufgabe_3.1/server.ts
@@ -1,11 +1,12 @@
 import * as Http from "http"; //import sämtlicher Funktionalitäten von "http"
+import * as Url from "url";
 
 
 export namespace P_3_1Server { 
     console.log("Starting server");    //Ausgabe zu Beginn des Serverstarts
     let port: number = Number(process.env.PORT);    //Ist mir nicht ganz klar
     if (!port)   
-        port = 8100;  // Setzt den Port auf 8100
+        port = 8101;  // Setzt den Port auf 8100
 
     let server: Http.Server = Http.createServer();  //Erstellt neuen http-Server
     server.addListener("request", handleRequest);   //"request-listener" für einkommende Anfragen an den Server -> bei einer Anfrage wird die Funktion "handleRequest ausgeführt"
@@ -19,10 +20,27 @@ export namespace P_3_1Server {
     // wird ausgeführt wenn der Server eine Anfrage bekommt
     function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
         console.log("I hear voices!");  //Wird ausgegeben sobald der Server von jemandem erreicht wurde und die handleRequest "aktiviert" wird
-        _response.setHeader("content-type", "text/html; charset=utf-8"); //Definiert die "Art" der response-Daten
+
+        _response.setHeader("content-type", "text/html; charset=utf-8");
         _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write(_request.url); //Zeigt die individuelle request-URL an.
-        console.log(_request.url); //Gibt die individuelle request-URL aus.
+
+        let urlparse: Url.UrlWithParsedQuery = Url.parse(_request.url, true);
+        let url: URL = new URL(_request.url, "https://yonysgisserver.herokuapp.com");
+
+        if (url.pathname == "/html") {
+            
+            for (let key in urlparse.query) {
+
+                console.log("html");
+                _response.write("<p>" + key + ":" + urlparse.query[key] + "<p/>");
+            }
+        }
+        else if (url.pathname == "/json") {
+
+            console.log("json");
+            let antwortJson: string = JSON.stringify(urlparse.query);
+            _response.write(antwortJson);
+        }
         _response.end();
     }
 }

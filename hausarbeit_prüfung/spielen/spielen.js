77c02b2b4c12850a392420bbcf3aca9d107fdabc
YonyM <yonymartens@gmx.de>
2021-07-11 20:03:44 +0200

    korrektur5

--- hausarbeit_prüfung/spielen/spielen.js
+++ hausarbeit_prüfung/spielen/spielen.js
@@ -81,7 +81,7 @@ var Memeory;
                         let zeitString = ((dateEnd.getTime() - date.getTime()) / 1000) + " Sek";
                         localStorage.setItem("zeit", zeitString);
                         localStorage.setItem("zeitEintragen", "ja");
-                        window.location.href = "https://yonym.github.io/GIS_SoSe_2021/hausarbeit_pr%C3%BCfung/scores/scores.html";
+                        window.location.href = "../hausarbeit_prüfung/scores/scores.html";
                     }
                 }
                 else {
